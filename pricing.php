037213da72f90af819637f27853e5775d7b8f825
tbedner <tdavidbedner@gmail.com>
2025-12-20 20:37:48 -0600

    Adding pricing

---
+++ pricing.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html>
+<?php
+include('head.php');
+?>
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+<?php
+include('header.php');
+include('menu.php');
+?>
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+<section class="atschool-course-comparison" lang="en">
+							<?php echo $translations['compare_title']; ?>
+
+  <table class="atschool-table">
+    <thead>
+      <tr>
+        <?php echo $translations['compare_tabletitle1']; ?>
+        <?php echo $translations['compare_tabletitle2']; ?>
+        <?php echo $translations['compare_tabletitle3']; ?>
+        <?php echo $translations['compare_tabletitle4']; ?>
+      </tr>
+    </thead>
+    <tbody style="text-align:center;">
+        <?php echo $translations['compare_table1']; ?>
+        <?php echo $translations['compare_table2']; ?>
+        <?php echo $translations['compare_table3']; ?>
+        <?php echo $translations['compare_table4']; ?>
+        <?php echo $translations['compare_table5']; ?>
+        <?php echo $translations['compare_table6']; ?>
+        <?php echo $translations['compare_table7']; ?>
+        <?php echo $translations['compare_table8']; ?>
+        <?php echo $translations['compare_table9']; ?>
+        <?php echo $translations['compare_table10']; ?>
+        <?php echo $translations['compare_table11']; ?>
+        <?php echo $translations['compare_table12']; ?>
+        <?php echo $translations['compare_table13']; ?>
+        <?php echo $translations['compare_table14']; ?>
+    </tbody>
+  </table>
+
+  <div class="atschool-course-blurbs">
+    <?php echo $translations['compare_courseblurb_main']; ?>
+    <?php echo $translations['compare_courseblurb_title1']; ?>
+    <?php echo $translations['compare_courseblurb_p1']; ?>
+    <?php echo $translations['compare_courseblurb_title2']; ?>
+    <?php echo $translations['compare_courseblurb_p2']; ?>
+    <?php echo $translations['compare_courseblurb_title3']; ?>
+    <?php echo $translations['compare_courseblurb_p3']; ?>   
+  </div>
+</section>
+
+						</div>
+					</div>
+
+<?php
+include('contact_footer.php');
+?>
+	</body>
+</html>
