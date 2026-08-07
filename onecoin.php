75c77998cf9a6095216b4e1118ca26faba89c508
tbedner <tdavidbedner@gmail.com>
2026-08-06 21:56:58 -0500

    styling updates

--- onecoin.php
+++ onecoin.php
@@ -14,11 +14,6 @@ include('menu.php');
 				<!-- Main -->
 					<div id="main">
 						<div class="inner">
-							<!-- <h1><?php echo $translations['join_title']; ?></h1> -->
-
-							<div class="image main">
-								<img src="images/blog-fullscreen-1-1920x700.jpg" class="img-fluid" alt="" />
-							</div>
                             
 <?php
 require_once __DIR__ . '/secrets.php';
