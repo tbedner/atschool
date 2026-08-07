da6134964db08611e082f030a05acfe82d1b8d11
tbedner <tdavidbedner@gmail.com>
2026-08-06 22:50:15 -0500

    redirecting to onecoin

--- join.php
+++ join.php
@@ -33,7 +33,7 @@ include('menu.php');
 
 							<p><?php echo $translations['join_p4']; ?></p>
 
-							<p class="text-center"><a href="moodle/login/signup.php?lang=<?php echo $lang; ?>" style="color:#fff;"><button type="button" class="btn btn-dark"><?php echo $translations['join_button']; ?></button></a></p>
+							<p class="text-center"><a href="onecoin.php?lang=<?php echo $lang; ?>" style="color:#fff;"><button type="button" class="btn btn-dark"><?php echo $translations['join_button']; ?></button></a></p>
 							
 							<p class="text-center"><a href="assets/docs/guide_<?php echo $lang; ?>.pdf" style="color:#fff;border-bottom:none !important;" target="_blank"><button type="button" class="btn" style="color:#585858 !important;"><?php echo $translations['join_docs']; ?></button></a></p>
 						</div>
