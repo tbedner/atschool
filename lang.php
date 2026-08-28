1ef815830e452c31d4f38380b0f00a27ef537ab4
tbedner <tdavidbedner@gmail.com>
2026-08-28 15:42:59 -0500

    bug fix

--- lang.php
+++ lang.php
@@ -2,7 +2,9 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-session_start();
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
 
 $acceptLanguageHeader = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '';
 
